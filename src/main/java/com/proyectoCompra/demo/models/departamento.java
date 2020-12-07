5d4eaa226b734ef9f0b8636def67c849be1cb90d
jessyrc <yessy2015rive@gmail.com>
2020-12-06 20:08:57 -0600

    Cambio de atributo nombre por nombre_depto y tipo de dato String a int

--- src/main/java/com/proyectoCompra/demo/models/departamento.java
+++ src/main/java/com/proyectoCompra/demo/models/departamento.java
@@ -1,34 +1,41 @@
 package com.proyectoCompra.demo.models;
 
-import javax.persistence.*;	
+import javax.persistence.*;
 
 @Entity
 @Table(name = "departamento")
 public class departamento {
 
-    public departamento() {
+	public departamento() {
 		super();
 	}
-	public departamento(long idDepto, String nombre) {
+
+	public departamento(long idDepto, String nombre_depto) {
 		super();
 		this.idDepto = idDepto;
-		this.nombre = nombre;
+		this.nombre_depto = nombre_depto;
 	}
+
 	@Id
-    @GeneratedValue(strategy = GenerationType.AUTO) private long idDepto;
-    @Column(name = "nombre") private String nombre;
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long idDepto;
+	@Column(name = "nombre_depto")
+	private String nombre_depto;
+
 	public long getIdDepto() {
 		return idDepto;
 	}
+
 	public void setIdDepto(long idDepto) {
 		this.idDepto = idDepto;
 	}
-	public String getNombre() {
-		return nombre;
-	}
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}  
 
-    
+	public String getnombre_depto() {
+		return nombre_depto;
+	}
+
+	public void setnombre_depto(String nombre_depto) {
+		this.nombre_depto = nombre_depto;
+	}
+
 }
--- src/main/java/com/proyectoCompra/demo/models/detalleCompra.java
+++ src/main/java/com/proyectoCompra/demo/models/detalleCompra.java
@@ -1,6 +1,7 @@
 package com.proyectoCompra.demo.models;
+
 import javax.persistence.*;
-	
+
 @Entity
 @Table(name = "detalleCompra")
 
@@ -8,8 +9,9 @@ public class detalleCompra {
 	public detalleCompra() {
 		super();
 	}
-	public detalleCompra(long idDetalleCompra, String cantidad, float total,
-			com.proyectoCompra.demo.models.compra compra, articuloProveedor articulo) {
+
+	public detalleCompra(long idDetalleCompra, int cantidad, float total, com.proyectoCompra.demo.models.compra compra,
+			articuloProveedor articulo) {
 		super();
 		this.idDetalleCompra = idDetalleCompra;
 		this.cantidad = cantidad;
@@ -17,41 +19,55 @@ public class detalleCompra {
 		this.compra = compra;
 		this.articulo = articulo;
 	}
+
 	@Id
-    @GeneratedValue(strategy = GenerationType.AUTO) private long idDetalleCompra;
-	@Column(name = "cantidad") private String cantidad;  
-	@Column(name = "total") private float total;  
-    @ManyToOne
-    private compra compra;
-    @ManyToOne
-    private articuloProveedor articulo;
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long idDetalleCompra;
+	@Column(name = "cantidad")
+	private int cantidad;
+	@Column(name = "total")
+	private float total;
+	@ManyToOne
+	private compra compra;
+	@ManyToOne
+	private articuloProveedor articulo;
+
 	public long getIdDetalleCompra() {
 		return idDetalleCompra;
 	}
+
 	public void setIdDetalleCompra(long idDetalleCompra) {
 		this.idDetalleCompra = idDetalleCompra;
 	}
-	public String getCantidad() {
+
+	public int getCantidad() {
 		return cantidad;
 	}
-	public void setCantidad(String cantidad) {
+
+	public void setCantidad(int cantidad) {
 		this.cantidad = cantidad;
 	}
+
 	public float getTotal() {
 		return total;
 	}
+
 	public void setTotal(float total) {
 		this.total = total;
 	}
+
 	public compra getCompra() {
 		return compra;
 	}
+
 	public void setCompra(compra compra) {
 		this.compra = compra;
 	}
+
 	public articuloProveedor getArticulo() {
 		return articulo;
 	}
+
 	public void setArticulo(articuloProveedor articulo) {
 		this.articulo = articulo;
 	}
