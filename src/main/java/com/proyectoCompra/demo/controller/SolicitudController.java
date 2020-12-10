0a0b82aa22929032c0147fa27479767dcc856078
jessyrc <yessy2015rive@gmail.com>
2020-12-09 21:59:32 -0600

    Generar orden de compra desde solicitudes

--- src/main/java/com/proyectoCompra/demo/controller/SolicitudController.java
+++ src/main/java/com/proyectoCompra/demo/controller/SolicitudController.java
@@ -15,15 +15,26 @@ public class SolicitudController {
     @Autowired
     private SolicitudRepository solicitudRepository;
 
+    // Listar las solicitudes(Requisiiones)
     @GetMapping("/requisicion-index")
     public String getHome(Pageable pageable, Model model) {
         model.addAttribute("solicitudes", solicitudRepository.findAll(pageable));
         return "requisiciones/index";
     }
 
+    // ver detalles de solicitud(Requisicion)
     @GetMapping("/requisicion-show/{id}")
     public ModelAndView getShow(@RequestParam(defaultValue = "0", required = false) int id, Pageable pageable) {
         ModelAndView modelAndView = new ModelAndView("requisiciones/show");
+        modelAndView.addObject("detalles", solicitudRepository.ObtenerById(id));
+        modelAndView.addObject("solicitud", solicitudRepository.findById(id));
+        return modelAndView;
+    }
+
+    // Generar orden de compra
+    @GetMapping("/orden-generate/{id}")
+    public ModelAndView getOrden(@RequestParam(defaultValue = "0", required = false) int id, Pageable pageable) {
+        ModelAndView modelAndView = new ModelAndView("requisiciones/GenerarOrden");
         modelAndView.addObject("detalles", solicitudRepository.ObtenerById(id));
         modelAndView.addObject("solicitud", solicitudRepository.findById(id));
         return modelAndView;
